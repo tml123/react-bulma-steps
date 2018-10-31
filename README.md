4ce5c4d3c14a333203b2ddbd6b611f7a9f34a980
Timothy Lanahan <timothy.lanahan@fts-intl.local>
2018-10-31 11:11:21 -0400

    init

---
+++ README.md
@@ -0,0 +1,19 @@
+# React Bulma Steps
+
+A component based on https://aramvisser.github.io/bulma-steps/.  The component uses the new ContextAPI and requires a version of react compatible with it.
+
+## To use:
+
+A distribution is not available, but feel free to grab the src, contribute, suggest, etc.
+
+Eventually, some content having to do with usage to go here....but something like this:
+
+```
+<Steps dashed={true} activeStep={`step${this.state.activeStep}`}>
+    <Step id={'step1'} marker={1} details={`Some details for step 1`}></Step>
+    <Step id={'step2'} marker={2} details={`Some details for step 2`}></Step>
+    <Step id={'step3'} marker={3} details={`Some details for step 3`}></Step>
+    <Step id={'step4'} marker={4} details={`Some details for step 4`}></Step>
+    <Step id={'step5'} marker={5} details={`Some details for step 5`}></Step>
+</Steps>
+```
---
+++ config/webpack.base.config.js
@@ -0,0 +1,50 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin'); 
+
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const merge = require('webpack-merge');
+const fs = require('fs');
+const webpack = require('webpack');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+const path = require('path');
+
+const APP_DIR = path.resolve(__dirname, '../src');
+
+module.exports = env => {
+    const { PLATFORM, VERSION } = env;
+    return merge([ {
+        entry: ['@babel/polyfill', APP_DIR],
+        module: {
+            rules: [
+                {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                        loader: 'babel-loader'
+                    }
+                },
+                {
+                    test: /\.(scss|sass)$/,
+                    use: [
+                        PLATFORM === 'production' ? MiniCssExtractPlugin.loader : 'style-loader',
+                        'css-loader',
+                        'sass-loader'
+                    ]
+                }
+            ]
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                template: './src/index.html',
+                filename: './index.html'
+            }),
+            new webpack.DefinePlugin({
+                'process.env.VERSION': JSON.stringify(env.VERSION),
+                'process.env.PLATFORM': JSON.stringify(env.PLATFORM),
+                'process.env.WEATHER_API_KEY': JSON.stringify(env.WEATHER_API_KEY)
+            }),
+            new CopyWebpackPlugin([ { from: 'src/static' } ])
+        ]
+        }
+    ])
+}
